cbefbcc1c22a8bbb0074ad7d7ec7e9e703a61a21
Zara <2019zding@dwight.edu>
2018-01-23 08:37:47 -0500

    first push

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'about', to: 'static_pages#about'
+
+  get 'contact', to: 'static_pages#contact'
+
+  get 'personalInterests', to: 'static_pages#personalInterests'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
